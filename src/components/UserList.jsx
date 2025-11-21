b54b8f965c65d2a11d108f29f0a2e73b1d504704
Josué Juarez <jjuarezm16@miumg.edu.gt>
2025-11-20 21:42:37 -0600

    Agregar archivo de configuración API y actualizar imports

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Pencil, Trash2 } from "lucide-react";
 import toast from "react-hot-toast";
+import { API_URL } from "../config/api";
 
 function UserList() {
   const [users, setUsers] = useState([]);
